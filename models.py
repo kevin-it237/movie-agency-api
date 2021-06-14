6f6783dc233a4a7df0ecc3aab8f175f0ad2358b8
kevin-it237 <ngaleuabel@gmail.com>
2021-06-15 00:23:30 +0100

    Add flask migrate

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ import os
 if os.environ['FLASK_ENV'] == 'production':
   database_path = os.environ['DATABASE_URL']
   if database_path.startswith("postgres://"):
-    uri = database_path.replace("postgres://", "postgresql://", 1)
+    database_path = database_path.replace("postgres://", "postgresql://", 1)
 else:
   database_name = "casting_agency_db"
   database_path = "postgresql://{}:{}@{}/{}".format('postgres', 'noelle','localhost:5432', database_name)
